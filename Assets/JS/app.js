e4356a7c5b3ffc18f4b27aeeb3405f1facac8619
joe tech <ojtech2003@gmail.com>
2022-06-28 10:32:45 +0100

    testimonials

---
+++ Assets/JS/app.js
@@ -0,0 +1,13 @@
+const toggleBtn = document.querySelector('.nav-toggle');
+const closeBtn = document.querySelector('.close-btn');
+const sidebar = document.querySelector('.sidebar');
+
+toggleBtn.addEventListener('click', function () {
+  // console.log(sidebar.classList);
+
+  sidebar.classList.toggle('show-sidebar');
+});
+
+closeBtn.addEventListener('click', function () {
+  sidebar.classList.remove('show-sidebar');
+});
